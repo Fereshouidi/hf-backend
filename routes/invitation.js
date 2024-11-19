6975365b3a3d85b0185e9e550e1c25cf2e54f6e3
Fereshouidi <151401384+Fereshouidi@users.noreply.github.com>
2024-11-19 12:41:27 +0100

    Update invitation.js

--- routes/invitation.js
+++ routes/invitation.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 const Invitation = require('../models/invitation');
 const User = require('../models/users');
-const Relation = require('./models/Relation');
+const Relation = require('../models/Relation');
 
 
 router.put('/accept/invitation', async (req, res) => {
